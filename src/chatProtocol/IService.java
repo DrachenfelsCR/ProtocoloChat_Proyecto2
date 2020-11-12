554b2b649f67959d2a2d65ed4ffdb7d8ef5d9abd
David <David@DESKTOP-28KAJJM>
2020-11-11 20:00:08 -0600

    Mensajes a contactos no existentes no se envian por el servidor

--- src/chatProtocol/IService.java
+++ src/chatProtocol/IService.java
@@ -4,4 +4,5 @@ public interface IService {
     public User login(User u) throws Exception;
     public void logout(User u) throws Exception; 
     public void post(PaqueteDatos message, String idEmisor, String idReceptor);
+    public User get(String id);
 }
